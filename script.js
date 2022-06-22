07ad503abff017ff4efb26f2fb31ce58ba3908ce
Izabela Żmujdzin <izabela.zmujdzin@gmail.com>
2022-06-22 15:40:09 +0200

    //Rolling dice funcionality

--- script.js
+++ script.js
@@ -3,9 +3,36 @@
 // Selecting element
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0EL = document.getElementById('curent--0');
+const current1EL = document.getElementById('curent--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice funcionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0EL.textContent = currentScore;
+  } else {
+    // Switch to nexr player
+  }
+});
